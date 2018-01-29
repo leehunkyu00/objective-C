9f06b475eddf184a54af8517c37b901e87c0f82c
leehunkyu00 <hklee@hkleeui-iMac.local>
2018-01-29 19:43:53 +0900

    Updated about displaying information for current price on window
        - This is foundation for mts current base.

--- tutorial/udemy/SwitchingXIBEnhance/SwitchingXIB/TableViewController.h
+++ tutorial/udemy/SwitchingXIBEnhance/SwitchingXIB/TableViewController.h
@@ -8,8 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
-@interface TableViewController : UIViewController <UITableViewDelegate, UITableViewDataSource> {
+@interface TableViewController : UIViewController {
     NSArray *datalist;
+    NSMutableArray *priceArray;         // in UITextView *
+    NSMutableArray *volumeArray;        // in UITextView *
+    NSMutableArray *pricePercentArray;  // in UITextView *
+    UITextView *stockbasePriceTextView;
+    
+    NSInteger stockBasedPrice;
 }
+@property (strong, nonatomic) IBOutlet UIScrollView *tab1priceScrollView;
+@property (strong, nonatomic) IBOutlet UIView *tab1priceView;
+
+- (void)update:(NSArray*)price andVolume:(NSArray*)volume;
 
 @end
--- tutorial/udemy/SwitchingXIBEnhance/SwitchingXIB/TableViewController.m
+++ tutorial/udemy/SwitchingXIBEnhance/SwitchingXIB/TableViewController.m
@@ -14,12 +14,77 @@
 @end
 
 @implementation TableViewController
+@synthesize tab1priceScrollView, tab1priceView;
 
 - (void)viewDidLoad {
     NSLog(@"TableViewController (void)viewDidLoad");
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     datalist = [[NSArray alloc] initWithObjects:@"123", @"234", @"345", @"456", @"567",nil];
+    
+    // UIViewText for price
+    priceArray = [[NSMutableArray alloc] initWithCapacity:0];
+    for (int i =0; i < 10; i++) {
+        CGRect textViewRect = CGRectMake(125.0, 0.0+48.0*i, 100.0, 48.0);
+        UITextView *uitextview = [[UITextView alloc] initWithFrame:textViewRect];
+        uitextview.text = @"hello world!";
+        [priceArray addObject:uitextview];
+        [tab1priceView addSubview:uitextview];
+    }
+    
+    // UIViewText for volume
+    volumeArray = [[NSMutableArray alloc] initWithCapacity:0];
+    for (int i = 0; i < 5; i++) {
+        CGRect textViewRect = CGRectMake(0.0, 0.0+48.0*i, 100.0, 48.0);
+        UITextView *uitextview = [[UITextView alloc] initWithFrame:textViewRect];
+        uitextview.text = @"hello world!";
+        [volumeArray addObject:uitextview];
+        [tab1priceView addSubview:uitextview];
+    }
+    for (int i = 5; i < 10; i++) {
+        CGRect textViewRect = CGRectMake(250.0, 0.0+48.0*i, 100.0, 48.0);
+        UITextView *uitextview = [[UITextView alloc] initWithFrame:textViewRect];
+        uitextview.text = @"hello world!";
+        [volumeArray addObject:uitextview];
+        [tab1priceView addSubview:uitextview];
+    }
+    
+    // UIViewText for Percent
+    pricePercentArray = [[NSMutableArray alloc] initWithCapacity:0];
+    for (int i =0; i < 10; i++) {
+        //CGRect textViewRect = CGRectMake(125.0, 0.0+48.0*i, 100.0, 48.0);
+        CGRect textViewRect = CGRectMake(200.0, 10.0+48.0*i, 50.0, 24.0);
+        UITextView *uitextview = [[UITextView alloc] initWithFrame:textViewRect];
+        uitextview.text = @"-10.00%";
+        [uitextview setFont:[UIFont systemFontOfSize:8]];
+
+        [pricePercentArray addObject:uitextview];
+        [tab1priceView addSubview:uitextview];
+    }
+    
+    // UIViewText for stock base price
+    //CGRect stockBasePriceTextViewRect = CGRectMake(375.0, 200.0, 100.0, 48.0);
+    CGRect stockBasePriceTextViewRect = CGRectMake(300.0, 10.0, 100.0, 48.0);
+    stockbasePriceTextView = [[UITextView alloc] initWithFrame:stockBasePriceTextViewRect];
+    stockbasePriceTextView.text =@"11111";
+    [tab1priceView addSubview:stockbasePriceTextView];
+    
+    UITextView *stockBasePriceLabelTextView = [[UITextView alloc] initWithFrame: CGRectMake(280.0, 10.0, 20.0, 48.0)];
+    stockBasePriceLabelTextView.text = @"시";
+    [tab1priceView addSubview:stockBasePriceLabelTextView];
+    
+    // setting position for scroll view
+    [tab1priceScrollView setContentOffset:CGPointMake(10.0, 140.0) animated:YES];
+    //[tab1priceScrollView scrollRectToVisible:CGRectMake(100, 140, 100, 100) animated:YES];
+    //tab1priceScrollView.contentOffset = CGPointMake(0, -200);
+
+    // test for update
+    //NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(update) userInfo:nil repeats:YES];
+    [NSTimer scheduledTimerWithTimeInterval:3.0 target:self selector:@selector(update) userInfo:nil repeats:YES];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [self.tab1priceScrollView setContentSize:CGSizeMake(0, 480)];       // setting scrollview size
 }
 
 - (void)didReceiveMemoryWarning {
@@ -27,55 +92,54 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    NSLog(@"(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath");
-    return [datalist count];
+- (void)update:(NSArray*)price andVolume:(NSArray*)volume {
+    /* TODO implements*/
 }
 
--(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    NSLog(@"(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath");
-    static NSString *identifier = @"PriceCell";
+- (void)update {
+    NSUInteger randValue = arc4random() % 900000;
+
+    randValue = randValue - randValue % 1000;
     
-    TableViewTextCell *cell;
-    cell = (TableViewTextCell *)[tableView dequeueReusableCellWithIdentifier:identifier];
+    stockBasedPrice = randValue - 5000; // for test
+
+    stockbasePriceTextView.text = [NSString stringWithFormat:@"%ld", (long)stockBasedPrice];
     
-    if (cell == nil) {
-        NSLog(@"CELL IS EMPTY");
-        /*
-        NSArray *array = [[NSBundle mainBundle] loadNibNamed:@"TableViewTextCell" owner:nil options:nil];
-        cell = [array objectAtIndex:0];
-        */
-        /*
-        [[NSBundle mainBundle] loadNibNamed:@"TableViewTextCell" owner:nil options:nil];
-        cell = _tableViewTextCell;
-        */
-        //cell = [[TableViewTextCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
-        //cell = [[TableViewTextCell alloc] init];
-    } else {
-        NSLog(@"CELL REUSE!");
+    for (int i = 0; i < 10; i++) {
+        NSInteger currentPrice = randValue - 1000*i;
+        CGFloat currentPricePercent = ((float)currentPrice - (float)stockBasedPrice)/(float)currentPrice*100.0; 
+        NSLog(@"%f", currentPricePercent);
+        
+        UITextView *curPriceTextView = ((UITextView *)[priceArray objectAtIndex:i]);
+        UITextView *curVolumeTextView = ((UITextView *)[volumeArray objectAtIndex:i]);
+        UITextView *curPricePercentTextView = ((UITextView *)[pricePercentArray objectAtIndex:i]);
+        //((UITextView *)[pricePercentArray objectAtIndex:i]).text = [NSString stringWithFormat:@"%lu", ];
+        
+        curPriceTextView.text = [NSString stringWithFormat:@"%lu", currentPrice];
+        curVolumeTextView.text = [NSString stringWithFormat:@"%lu", currentPrice];  // just test
+        curPricePercentTextView.text = [NSString stringWithFormat:@"%.2f%%", currentPricePercent];  // just test
+
+        if (stockBasedPrice > currentPrice) {
+            // color : blue
+            [curPricePercentTextView setTextColor:[UIColor blueColor]];
+            [curPriceTextView setTextColor:[UIColor blueColor]];
+
+        } else if (stockBasedPrice < currentPrice) {
+            // color : red
+            [curPricePercentTextView setTextColor:[UIColor redColor]];
+            [curPriceTextView setTextColor:[UIColor redColor]];
+            
+        } else if (stockBasedPrice == currentPrice) {
+            // color : black
+            [curPricePercentTextView setTextColor:[UIColor blackColor]];
+            [curPriceTextView setTextColor:[UIColor blackColor]];
+            
+        }
+        //pricePercent =    // TODO implements
+//        ((UITextView *)[priceArray objectAtIndex:i]).text = randStr;
+//        ((UITextView *)[volumeArray objectAtIndex:i]).text = randStr;
+        //text = arc4random() % 30000;
     }
-    /*
-    UILabel *mylabel = [[UILabel alloc] initWithFrame:CGRectMake(35,13, 230, 30)];
-    [cell.contentView addSubview:mylabel];
-    mylabel.text = @"Hello";
-    */
-
-    [cell setText:[datalist objectAtIndex:indexPath.row]];
-    
-//    cell = [[TableViewTextCell alloc] init];
-//    [cell setText:datalist[indexPath.row]];
-    
-    return cell;
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
